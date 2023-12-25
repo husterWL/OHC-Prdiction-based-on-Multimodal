1cad9c2633ea077323474d23ceee9c9632fc838b
hustwl159 <hustwl159@qq.com>
2023-12-25 16:14:48 +0800

    2023年12月25日，scrapy测试爬取医管院信息管理系的信息

--- spider/haodf_spider_wl/haodf_spider_wl/items.py
+++ spider/haodf_spider_wl/haodf_spider_wl/items.py
@@ -10,6 +10,6 @@ import scrapy
 class HaodfSpiderWlItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    
     name = scrapy.Field()
     
--- spider/haodf_spider_wl/haodf_spider_wl/spiders/test.py
+++ spider/haodf_spider_wl/haodf_spider_wl/spiders/test.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from haodf_spider_wl.items import HaodfSpiderWlItem
 
 class TestSpider(scrapy.Spider):
     name = 'test'
@@ -8,5 +8,22 @@ class TestSpider(scrapy.Spider):
 
     #解析的方法，每个初始URL完成下载后将被调用，调用的时候传入从每一个URL传回的Response对象来作为唯一参数
     def parse(self, response):
-        filename = 'ygyxinguanlaoshi.html'
-        open(filename, 'w', encoding = 'UTF-8').write(response.text)
+        # filename = 'ygyxinguanlaoshi.html'
+        # open(filename, 'w', encoding = 'UTF-8').write(response.text)
+        context = response.xpath('/html/head/title/text()')
+        title = context.extract_first()   #extract()返回列表，extract_first()返回字符串
+        print(title)
+        # print(type(title))
+        items = []
+        print(response.xpath('/html/body/div[5]/div/div[2]//div/div/ul/li/a//p/text()'))
+        for i in response.xpath('/html/body/div[5]/div/div[2]//div/div/ul/li/a//p/text()').extract():   #不使用extract()函数，会导致提取的姓名只有第一个字
+            pass
+            item = HaodfSpiderWlItem()
+            # name = i.extract()    #返回的是列表
+            
+            item['name'] = i
+
+            items.append(item)
+        print(items)
+        return items
+    #打印医管信息管理系老师的姓名
